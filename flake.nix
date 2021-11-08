eaf04e253a51b78c4215804a0d9d97fb3c26718b
Toby Rea <40074404+Toby-Rea@users.noreply.github.com>
2021-11-08 16:36:31 +0000

    formatting

--- flake.nix
+++ flake.nix
@@ -1,31 +1,31 @@
 {
   description = "Personal system configuration";
-
+  
   inputs = {
     # Nixpkgs
     unstable.url = "github:nixos/nixpkgs/nixos-unstable";
     nixpkgs.follows = "unstable";
     home-manager.url = "github:nix-community/home-manager";
-
+    
     # Overlays
     emacs.url = "github:nix-community/emacs-overlay";
     nixpkgs-wayland.url = "github:nix-community/nixpkgs-wayland";
   };
-
+  
   outputs = inputs @ { self, nixpkgs, home-manager, ... }:
   let
     system = "x86_64-linux";
     pkgs = import nixpkgs {
       inherit system;
-
+      
       config = { 
         allowUnfree = true;
-				allowBroken = true;
+        allowBroken = true;
       };
       
       overlays = with inputs; [
         emacs.overlay
-				nixpkgs-wayland.overlay
+        nixpkgs-wayland.overlay
       ];
     };
   in {
@@ -47,12 +47,12 @@
       thinkpad = nixpkgs.lib.nixosSystem {
         inherit system pkgs;
         modules = [ 
-	  			./thinkpad
-	  			home-manager.nixosModules.home-manager {
+          ./thinkpad
+          home-manager.nixosModules.home-manager {
             home-manager.useGlobalPkgs = true;
-	    			home-manager.useUserPackages = true;
-	    			home-manager.users.toby = import ./thinkpad/home.nix;
-	  			}
+            home-manager.useUserPackages = true;
+            home-manager.users.toby = import ./thinkpad/home.nix;
+          }
         ];
       };
     };
