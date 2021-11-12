c37dcd6fa7e417eb25f7f236d9eb203176aafd14
Toby Rea <toby.e.rea@protonmail.com>
2021-11-12 16:15:24 +0000

    Refined config

--- thinkpad/config/mako.nix
+++ thinkpad/config/mako.nix
@@ -2,10 +2,21 @@
 
   programs.mako = {
     enable = true;
-    width = 340;
+    
+    font = "monospace 12";
+    anchor = "top-right";
+    layer = "top";
+        
+    width = 400;
+    height = 140;
+    borderSize = 3;
+    margin = "8";
+    padding = "16";
+    
     backgroundColor = "#FFFFFFFF";
     textColor = "#161616FF";
-    padding = "16";
+    borderColor = "#D0D0D0FF";
+    
     defaultTimeout = 4000;
   };
 
