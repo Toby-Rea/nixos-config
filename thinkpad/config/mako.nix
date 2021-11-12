7e38dba728bd878b951ee8deaa65cda95fbf1af5
Toby Rea <toby.e.rea@protonmail.com>
2021-11-12 11:47:44 +0000

    Updated notifications config.

--- thinkpad/config/mako.nix
+++ thinkpad/config/mako.nix
@@ -2,6 +2,11 @@
 
   programs.mako = {
     enable = true;
+    width = 340;
+    backgroundColor = "#FFFFFFFF";
+    textColor = "#161616FF";
+    padding = "16";
+    defaultTimeout = 4000;
   };
 
 }
