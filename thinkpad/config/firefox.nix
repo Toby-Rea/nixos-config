a0bbfebbc832e6f45d53315f3446b2545194a5b1
Toby Rea <toby.e.rea@protonmail.com>
2021-11-14 21:17:25 +0000

    Revert (broken)

--- thinkpad/config/firefox.nix
+++ thinkpad/config/firefox.nix
@@ -2,12 +2,6 @@
 
   programs.firefox = {
     enable = true;
-    package = pkgs.wrapFirefox pkgs.firefox-unwrapped {
-      forceWayland = true;
-      extraPolicies = {
-        ExtensionSettings = {};
-      };
-    };
   };
   
 }
